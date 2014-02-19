0242dd7c28556b28483b875c68fe823a273c6616
Jerry Sievert <code@legitimatesounding.com>
2014-02-19 05:02:55 -0800

    more tests and some code reorganization

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # http
 
 A completely experimental implementation of an http server implemented against Node in pure Javascript.
-
-The goal is to implement post-http.
--- test/http-parser-test.js
+++ test/http-parser-test.js
@@ -6,6 +6,24 @@ var parse = require(__dirname + '/../lib/http_parser');
 vows.describe("parser test").addBatch({
   'when passed a fully ready to parse header': {
     topic: function () {
+      return parse("GET /index.html HTTP/1.1\r\nHeader1: foo\r\nHeader2: bar\r\n\r\n");
+    },
+    'method should parse': function (topic) {
+      assert.equal(topic.method, "GET");
+    },
+    'version should equal 1.1': function (topic) {
+      assert.equal(topic.version, "1.1");
+    },
+    'headers should parse correctly': function (topic) {
+      assert.equal(topic.headers['Header1'], 'foo');
+      assert.equal(topic.headers['Header2'], 'bar');
+    },
+    'url should be correct': function (topic) {
+      assert.equal(topic.url, '/index.html');
+    }
+  },
+  'when passed a full http request': {
+    topic: function () {
       return parse("GET /index.html HTTP/1.1\r\nHeader1: foo\r\nHeader2: bar\r\n\r\ndata");
     },
     'method should parse': function (topic) {
@@ -13,6 +31,30 @@ vows.describe("parser test").addBatch({
     },
     'version should equal 1.1': function (topic) {
       assert.equal(topic.version, "1.1");
+    },
+    'headers should parse correctly': function (topic) {
+      assert.equal(topic.headers['Header1'], 'foo');
+      assert.equal(topic.headers['Header2'], 'bar');
+    },
+    'url should be correct': function (topic) {
+      assert.equal(topic.url, '/index.html');
+    },
+    'the fragment should be returned': function (topic) {
+      assert.equal(topic.fragment, 'data');
+    }
+  },
+  'when passed a partial http request': {
+    topic: function () {
+      return parse("GET /index.html HTTP/1.1\r\nHeader1: foo\r\nHeader2: bar\r\n");
+    },
+    'not ready status should be returned': function (topic) {
+      assert.equal(topic.status, 'not ready');
+    },
+    'the fragment should be returned': function (topic) {
+      assert.equal(topic.fragment, 'GET /index.html HTTP/1.1\r\nHeader1: foo\r\nHeader2: bar\r\n');
+    },
+    'the type should still be parser': function (topic) {
+      assert.equal(topic.type, 'GET');
     }
   }
 }).export(module);
