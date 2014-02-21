288a7601a700b687d163bcf8884daaee38232e9d
Jerry Sievert <code@legitimatesounding.com>
2014-02-20 21:17:51 -0800

    more tests passing, more test coverage

--- test/incoming-message-test.js
+++ test/incoming-message-test.js
@@ -16,5 +16,20 @@ vows.describe("incoming message test").addBatch({
     'the header is parsed and a connect is emitted': function (topic) {
       assert.equal(topic.url, '/index.html');
     }
+  },
+  'when dealing with a valid request that has a header and data': {
+    topic: function () {
+      var packet = fs.createReadStream(__dirname + '/input/request_with_header_and_data.txt');
+      var message = new IncomingMessage(packet);
+
+      var self = this;
+      message.on("connect", function (data) { self.callback(null, data); });
+    },
+    'the header is parsed and a connect is emitted': function (topic) {
+      assert.equal(topic.url, '/index.html');
+    },
+    'the read returns the data': function (topic) {
+      assert.equal(topic.read(), 'Some data!');
+    }
   }
 }).export(module);
